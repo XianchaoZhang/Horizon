56ffcd05fae0f8cfbc0692477b1b2a96a953ac95
Prikshet <pshar10@u.rochester.edu>
2018-06-05 23:18:41 -0400

    complex testing

--- denice.cpp
+++ denice.cpp
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <complex.h>
+#include <complex>
 
 #define L 1
 #define W 1
 
+using namespace std;
 using namespace Halide;
 using namespace ConciseCasts;
 
 int main(int argc, char** argv) {
+	complex<double> num = polar(5, 50);
+	cout<< "num = "<< num << endl;
 	Buffer<uint8_t> input = Tools::load_image(argv[1]);
+
+	
 
 	
 }
