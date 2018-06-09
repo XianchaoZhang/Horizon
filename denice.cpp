ed7ce5f588d95016042ed33e01c9a1cf6eb58f11
Prikshet <pshar10@u.rochester.edu>
2018-06-08 21:18:59 -0400

    added denoise1.cpp

--- denice.cpp
+++ denice.cpp
@@ -1,7 +1,16 @@
 #include <stdio.h>
-#include <stdlib>
+#include <stdlib.h>
 #include <math.h>
+#include "Halide.h"
+#include "halide_image_io.h"
+#include "denoise.h"
 #include <complex>
+#define L 1
+#define W 1
+
+using namespace Halide;
+using namespace Halide::ConciseCasts;
+
 
 #define L 1
 #define W 1
@@ -11,8 +20,6 @@ using namespace Halide;
 using namespace ConciseCasts;
 
 int main(int argc, char** argv) {
-	complex<double> num = polar(5, 50);
-	cout<< "num = "<< num << endl;
 	Buffer<uint8_t> input = Tools::load_image(argv[1]);
 
 	
