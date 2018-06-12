efedff0ef4927e1b1859e9e9792503170c1172ca
Prikshet <pshar10@u.rochester.edu>
2018-06-12 16:09:36 -0400

    denoise_no_approx finished. Needs optimization.

--- pipeline/denoise_no_approx.cpp
+++ pipeline/denoise_no_approx.cpp
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
 #include <complex>
 #include "Halide.h"
 #include "halide_image_io.h"
 #include "denoise.h"
-#define L 10
-#define W 2
+#include <time.h>
+
 
 
 using namespace Halide;
@@ -16,12 +15,18 @@ using namespace std;
 
 int main(int argc, char **argv) {
 
+	clock_t time;
+	time = clock();
+
 	Buffer<uint8_t> input = Tools::load_image(argv[1]);
+
+
 		
 	printf("channels=%d\n", input.channels());
 	float sig_s_f = atof(argv[2]);
 	float sig_r_f = atof(argv[3]);
-	float epsi_f  = atof(argv[4]);
+	int L = sig_r_f * 3;
+	int W = sig_s_f * 3;
 
 	Var x, y, c, t;
 
@@ -36,31 +41,36 @@ int main(int argc, char **argv) {
 	g_sig_s(omega.x, omega.y) = f32(exp(-(omega.x * omega.x + omega.y * omega.y) / (2 * sig_s_f * sig_s_f)));
 
 	g_sig_r(t) = f32(exp(- t * t / (2 * sig_r_f * sig_r_f)));
-	//g_sig_r(t) = t;
 
-	Func imp_bi_filter, imp_bi_filter_num, imp_bi_filter_den;
 
+	Func imp_bi_filter, imp_bi_filter_num, imp_bi_filter_den, imp_bi_filter_num_clamped, imp_bi_filter_den_clamped;
+
+
+	Func box_filtered;
+
+	box_filtered(x, y, c) = u8((float)(1) / ((2 * L + 1) * (2 * L + 1)) * sum(input(x - l_r.x, y - l_r.y, c)));
 	
 	// Compute box filtered image
-	Buffer<uint8_t> box_filtered(input.width(), input.height(), input.channels()); 
+	Expr clamped_x = clamp(x, L, input.width() - 2 * L - 1);
+	Expr clamped_y = clamp(y, L, input.height() - 2 * L - 1);
 
-	Func box_filtered_test, foo;
-
-	box_filtered(x, y, c) = (f32((1 / ((2 * L + 1) * (2 * L + 1))) * sum(input(x - l_r.x, y - l_r.y, c))));
-
-	Buffer<uint8_t> box_filtered = foo.realize(input.width(), input.height(), input.channels());
-	box_filtered_test(x, y, c) = print(box_filtered(x, y, c));
-	imp_bi_filter_num(x, y, c) = u8(min(sum(g_sig_s(omega.x, omega.y) * g_sig_r(box_filtered(x - omega.x, y - omega.y, c) - box_filtered(x, y, c)) * input(x - omega.x, y - omega.y, c)), 255));
-	imp_bi_filter_den(x, y, c) = (u8(sum(g_sig_s(omega.x, omega.y) * (g_sig_r((box_filtered(x - omega.x, y - omega.y, c) - box_filtered(x, y, c)))))));
-	imp_bi_filter(x, y, c) = u8(imp_bi_filter_num(x, y, c) / imp_bi_filter_den(x, y, c));
-	// // imp_bi_filter.trace_stores();
+	imp_bi_filter_num(x, y, c) = f32(sum(g_sig_s(omega.x, omega.y) * g_sig_r(box_filtered(x - omega.x, y - omega.y, c) - box_filtered(x, y, c)) * input(x - omega.x, y - omega.y, c)));
+	imp_bi_filter_den(x, y, c) = f32(sum((g_sig_s(omega.x, omega.y)) * g_sig_r((box_filtered(x - omega.x, y - omega.y, c) - box_filtered(x, y, c)))));
+	imp_bi_filter_num_clamped(x, y, c) = imp_bi_filter_num(clamped_x, clamped_y, c);
+	imp_bi_filter_den_clamped(x, y, c) = imp_bi_filter_den(clamped_x, clamped_y, c);
+	
+	imp_bi_filter(x, y, c) = u8(imp_bi_filter_num_clamped(x, y, c) / imp_bi_filter_den_clamped(x, y, c));
+	//imp_bi_filter.trace_stores()
 
 	Buffer<uint8_t> shifted(input.width() - 2 * W, input.height() - 2 * W, input.channels());
 	shifted.set_min(W, W);
 
-	box_filtered_test.realize(shifted);
-	//imp_bi_filter.realize(shifted);
+	imp_bi_filter.realize(shifted);
 
 	Tools::save_image(shifted, "output.png");
+
+	time = clock() - time;
+
+	printf("runtime = %f\n", (float) time / CLOCKS_PER_SEC);
 
 }
